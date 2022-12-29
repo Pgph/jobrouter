6cf0c6e8e938a0b64a5ac811cb5175d45618b8f1
patrykborkowskitukano <patryk.borkowski@tukano.com.pl>
2022-12-29 01:30:26 +0100

    Added react router and subpages

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+export default function About() {
+    return (
+        <div className="about">
+            <h2>About us</h2>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore error provident aliquid dolor sapiente nisi non magni! Placeat, fugit a ut molestias consequuntur adipisci quos dignissimos, harum nobis dolorem quidem!
+                Magni aliquam reiciendis error veritatis odit ad voluptates, Ratione blanditiis tenetur sequi dolorum nam repellendus sed, delectus quas recusandae iure facilis debitis velit vel, ipsa dolorem nulla. Mollitia ex ab dolorum!
+            </p>
+            <p>
+                Lorem ipsum dolor sit,  nesciunt cumque vero inventore earum? Aperiam incidunt alias, deleniti, ratione eos perferendis corrupti voluptatibus, porro ex reiciendis rem nulla commodi suscipit accusamus?
+                Quidem dolorem itaque sed eveniet magnam pariatur delectus veniam inventore temporibus corrupti velit eligendi, mollitia animi possimus non! Nihil ea illo eos error ipsa fugit accusamus cupiditate! Quaerat, ea omnis!
+                Velit, harum voluptas architecto soluta perspiciatis nulla. Ratione blanditiis tenetur sequi dolorum nam repellendus sed, delectus quas recusandae iure facilis debitis velit vel, ipsa dolorem nulla. Mollitia ex ab dolorum!
+            </p>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore error provident aliquid dolor sapiente nisi non magni! Placeat, fugit a ut molestias consequuntur adipisci quos dignissimos, harum nobis dolorem quidem!
+                Magni aliquam  soluta perspiciatis nulla. Ratione blanditiis tenetur sequi dolorum nam repellendus sed, delectus quas recusandae iure facilis debitis velit vel, ipsa dolorem nulla. Mollitia ex ab dolorum!
+            </p>
+        </div>
+    )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,25 @@
+export default function Home() {
+    return (
+        <div className="home">
+            <h2>Welcome</h2>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore error provident aliquid dolor sapiente nisi non magni! Placeat, fugit a ut molestias consequuntur adipisci quos dignissimos, harum nobis dolorem quidem!
+                Magni aliquam reiciendis error veritatis odit ad voluptates, nesciunt cumque vero inventore earum? Aperiam incidunt alias, deleniti, ratione eos perferendis corrupti voluptatibus, porro ex reiciendis rem nulla commodi suscipit accusamus?
+                Quidem dolorem itaque sed eveniet magnam pariatur delectus veniam inventore temporibus corrupti velit eligendi, mollitia animi possimus non! Nihil ea illo eos error ipsa fugit accusamus cupiditate! Quaerat, ea omnis!
+                Velit, harum voluptas architecto soluta perspiciatis nulla. Ratione blanditiis tenetur sequi dolorum nam repellendus sed, delectus quas recusandae iure facilis debitis velit vel, ipsa dolorem nulla. Mollitia ex ab dolorum!
+            </p>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore error provident aliquid dolor sapiente nisi non magni! Placeat, fugit a ut molestias consequuntur adipisci quos dignissimos, harum nobis dolorem quidem!
+                Magni aliquam reiciendis error veritatis odit ad voluptates, nesciunt cumque vero inventore earum? Aperiam incidunt alias, deleniti, ratione eos perferendis corrupti voluptatibus, porro ex reiciendis rem nulla commodi suscipit accusamus?
+                Quidem dolorem itaque sed eveniet magnam pariatur delectus veniam inventore temporibus corrupti velit eligendi, mollitia animi possimus non! Nihil ea illo eos error ipsa fugit accusamus cupiditate! Quaerat, ea omnis!
+                Velit, harum voluptas architecto soluta perspiciatis nulla. Ratione blanditiis tenetur sequi dolorum nam repellendus sed, delectus quas recusandae iure facilis debitis velit vel, ipsa dolorem nulla. Mollitia ex ab dolorum!
+            </p>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore error provident aliquid dolor sapiente nisi non magni! Placeat, fugit a ut molestias consequuntur adipisci quos dignissimos, harum nobis dolorem quidem!
+                Magni aliquam reiciendis error veritatis odit ad voluptates, nesciunt cumque vero inventore earum? Aperiam incidunt alias, deleniti, ratione eos perferendis corrupti voluptatibus, porro ex reiciendis rem nulla commodi suscipit accusamus?
+                Quidem dolorem itaque sed eveniet magnam pariatur delectus veniam inventore temporibus corrupti velit eligendi, mollitia animi possimus non! Nihil ea illo eos error ipsa fugit accusamus cupiditate! Quaerat, ea omnis!
+                Velit, harum voluptas architecto soluta perspiciatis nulla. Ratione blanditiis tenetur sequi dolorum nam repellendus sed, delectus quas recusandae iure facilis debitis velit vel, ipsa dolorem nulla. Mollitia ex ab dolorum!
+            </p>
+        </div>
+    )
+}
