a68b3c10afd999cef3fd91cd1a612de6a68f02fb
patrykborkowskitukano <patryk.borkowski@tukano.com.pl>
2022-12-29 21:08:09 +0100

    Added new routes help and nested subpages

--- src/layouts/RootLayout.js
+++ src/layouts/RootLayout.js
@@ -8,6 +8,7 @@ export default function RootLayout() {
                     <h1>Jobrouter</h1>
                     <NavLink to='/'>Home</NavLink>
                     <NavLink to='about'>About</NavLink>
+                    <NavLink to='Help'>Help</NavLink>
                 </nav>
             </header>
             <main>
---
+++ src/pages/help/Faq.js
@@ -0,0 +1,37 @@
+export default function Faq() {
+    return (
+        <div className="faq">
+            <h3>Frequently Asked Questions</h3>
+
+            <div className="question">
+                <p><strong>Lorem ipsum dolor sit amet.</strong></p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ipsam illo eligendi esse tempore dignissimos nostrum magni ipsa rem aliquid!</p>
+            </div>
+
+            <div className="question">
+                <p><strong>Lorem ipsum dolor sit amet.</strong></p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ipsam illo eligendi esse tempore dignissimos nostrum magni ipsa rem aliquid!</p>
+            </div>
+
+            <div className="question">
+                <p><strong>Lorem ipsum dolor sit amet.</strong></p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ipsam illo eligendi esse tempore dignissimos nostrum magni ipsa rem aliquid!</p>
+            </div>
+
+            <div className="question">
+                <p><strong>Lorem ipsum dolor sit amet.</strong></p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ipsam illo eligendi esse tempore dignissimos nostrum magni ipsa rem aliquid!</p>
+            </div>
+
+            <div className="question">
+                <p><strong>Lorem ipsum dolor sit amet.</strong></p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ipsam illo eligendi esse tempore dignissimos nostrum magni ipsa rem aliquid!</p>
+            </div>
+
+            <div className="question">
+                <p><strong>Lorem ipsum dolor sit amet.</strong></p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ipsam illo eligendi esse tempore dignissimos nostrum magni ipsa rem aliquid!</p>
+            </div>
+        </div>
+    )
+}
