7124b404fca7598b0a60f60825b849171867c159
patrykborkowskitukano <patryk.borkowski@tukano.com.pl>
2022-12-30 11:50:27 +0100

    Added custon 404 page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Faq from './pages/help/Faq'
 import Contact from './pages/help/Contact'
+import NotFound from './pages/NotFound'
 
 //layouts
 import RootLayout from './layouts/RootLayout'
@@ -19,6 +20,7 @@ const router = createBrowserRouter(
         <Route path='faq' element={<Faq />} />
         <Route path='contact' element={<Contact />} />
       </Route>
+      <Route path="*" element={<NotFound />} />
     </Route>
   )
 )
